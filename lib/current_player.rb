c5b62fe4a5f93bf32e4fca1dc8caff7104c1a91a
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-14 13:44:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |slot|
-    if (slot == " " || slot == "" || slot == nil)
+    if (slot != " " && slot != "" && slot != nil)
       counter += 1
     end
   end
